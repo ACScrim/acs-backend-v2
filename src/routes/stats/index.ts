f67b2b94fba77048fd80a74d8211315ec371be94
Aubrian <aubriduhay@gmail.com>
2025-10-14 02:18:35 +0200

    Route stats accueil

---
+++ src/routes/stats/index.ts
@@ -0,0 +1,23 @@
+import { FastifyPluginAsync } from "fastify";
+
+const statsRoutes: FastifyPluginAsync = async (fastify) => {
+  fastify.get("/home", async (req, res) => {
+    const tournaments = await fastify.models.Tournament.countDocuments({
+      finished: true
+    });
+    const users = await fastify.models.User.countDocuments();
+    const uniqueGames = await fastify.models.Tournament.aggregate([
+      { $match: { finished: true } },
+      { $group: { _id: '$gameId' } },
+      { $count: 'totalUniqueGames' }
+    ]);
+
+    res.send({
+      tournaments,
+      users,
+      gamesPlayed: uniqueGames[0]?.totalUniqueGames || 0
+    });
+  });
+}
+
+export default statsRoutes;
