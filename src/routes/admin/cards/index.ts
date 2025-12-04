323dfd64004dbb0518ae13792732c4b8cbd71cb3
Aubrian <aubriduhay@gmail.com>
2025-12-04 14:05:51 +0100

    Créateur de carte partie admin

--- src/routes/admin/cards/index.ts
+++ src/routes/admin/cards/index.ts
@@ -1,5 +1,7 @@
 import {FastifyPluginAsync} from "fastify";
 import {adminGuard} from "../../../middleware/authGuard";
+import {ICard} from "../../../models/Card";
+import {IUser} from "../../../models/User";
 
 const cardsAdminRoutes: FastifyPluginAsync = async (fastify) => {
 
@@ -9,6 +11,49 @@ const cardsAdminRoutes: FastifyPluginAsync = async (fastify) => {
     return cards;
   });
 
+  fastify.post('/:id/approve', { preHandler: [adminGuard] }, async (req, resp) => {
+    const cardId = (req.params as any).id as string;
+    const card = await fastify.models.Card.findById(cardId);
+    if (!card) {
+      resp.status(404);
+      return { message: 'Carte non trouvée.' };
+    }
+
+    const creator = await fastify.models.User.findById(card.createdBy) as IUser;
+
+    await fastify.discordService.sendPrivateMessageCardApproval(
+      creator.discordId,
+      card as ICard
+    );
+
+    card.status = 'waiting';
+
+    await card.save();
+
+    return fastify.models.Card.findById(card.id).populate('frontAsset borderAsset createdBy');
+  });
+
+  fastify.post('/:id/reject', { preHandler: [adminGuard] }, async (req, resp) => {
+    const cardId = (req.params as any).id as string;
+    const card = await fastify.models.Card.findById(cardId);
+    if (!card) {
+      resp.status(404);
+      return { message: 'Carte non trouvée.' };
+    }
+
+    const creator = await fastify.models.User.findById(card.createdBy) as IUser;
+
+    await fastify.discordService.sendPrivateMessageCardRejected(
+      creator.discordId,
+      card as ICard
+    );
+
+    card.status = 'inactive';
+
+    await card.save();
+
+    return fastify.models.Card.findById(card.id).populate('frontAsset borderAsset createdBy');
+  })
 }
 
 export default cardsAdminRoutes;
