bc6d308089956146efe3224e3f3e754509aaadc5
Aubrian <aubriduhay@gmail.com>
2026-01-13 00:25:33 +0100

    Test correction prod

--- src/utils/MongoStore.ts
+++ src/utils/MongoStore.ts
@@ -1,44 +1,90 @@
 import { SessionStore } from '@fastify/session';
-import { MongoClient } from 'mongodb';
+import { MongoClient, Collection, Db } from 'mongodb';
+
+interface SessionDocument {
+  _id: string;
+  session: string; // JSON string, pas un objet
+  expires: Date;
+}
 
 class MongoSessionStore implements SessionStore {
   private client: MongoClient;
-  private collection: any;
+  private db: Db;
+  private collection!: Collection<SessionDocument>;
+  private connected: boolean = false;
 
   constructor(mongoUrl: string, dbName: string) {
     this.client = new MongoClient(mongoUrl);
-    this.collection = this.client.db(dbName).collection('sessions');
+    this.db = this.client.db(dbName);
+    this.init();
   }
 
-  async set(sessionId: string, session: any, callback: (err?: any) => void) {
-    try {
-      await this.collection.updateOne(
-        { _id: sessionId },
-        { $set: { session, expires: session.cookie.expires } },
-        { upsert: true }
-      );
-      callback();
-    } catch (err) {
-      callback(err);
+  private async init() {
+    await this.client.connect();
+    this.collection = this.db.collection<SessionDocument>('sessions');
+    await this.collection.createIndex({ expires: 1 }, { expireAfterSeconds: 0 });
+    this.connected = true;
+  }
+
+  private async ensureConnected(): Promise<any> {
+    if (!this.connected) {
+      await new Promise(resolve => setTimeout(resolve, 100));
+      return this.ensureConnected();
     }
   }
 
-  async get(sessionId: string, callback: (err: any, session?: any) => void) {
-    try {
-      const doc = await this.collection.findOne({ _id: sessionId });
-      callback(null, doc?.session || null);
-    } catch (err) {
-      callback(err);
-    }
+  set(sessionId: string, session: any, callback: (err?: any) => void) {
+    this.ensureConnected().then(async () => {
+      try {
+        const expires = session.cookie?.expires
+          ? new Date(session.cookie.expires)
+          : new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+
+        await this.collection.updateOne(
+          { _id: sessionId },
+          {
+            $set: {
+              session: JSON.stringify(session), // Stocker comme string JSON
+              expires
+            }
+          },
+          { upsert: true }
+        );
+        callback();
+      } catch (err) {
+        callback(err);
+      }
+    });
   }
 
-  async destroy(sessionId: string, callback: (err?: any) => void) {
-    try {
-      await this.collection.deleteOne({ _id: sessionId });
-      callback();
-    } catch (err) {
-      callback(err);
-    }
+  get(sessionId: string, callback: (err: any, session?: any) => void) {
+    this.ensureConnected().then(async () => {
+      try {
+        const doc = await this.collection.findOne({ _id: sessionId });
+        if (!doc) {
+          callback(null, null);
+          return;
+        }
+        // Parser le JSON string
+        const session = typeof doc.session === 'string'
+          ? JSON.parse(doc.session)
+          : doc.session;
+        callback(null, session);
+      } catch (err) {
+        callback(err);
+      }
+    });
+  }
+
+  destroy(sessionId: string, callback: (err?: any) => void) {
+    this.ensureConnected().then(async () => {
+      try {
+        await this.collection.deleteOne({ _id: sessionId });
+        callback();
+      } catch (err) {
+        callback(err);
+      }
+    });
   }
 }
 
