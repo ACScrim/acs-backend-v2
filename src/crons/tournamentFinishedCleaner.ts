82365b271c2e2fba457479346409c8da7dd08284
Aubrian <aubriduhay@gmail.com>
2026-01-24 15:18:09 +0100

    Nettoyage auto des salons et rôles discord après la fin du tournoi

--- src/crons/tournamentFinishedCleaner.ts
+++ src/crons/tournamentFinishedCleaner.ts
@@ -1,7 +1,8 @@
 import {log} from "../utils/utils";
 import {FastifyInstance} from "fastify";
-import {ITournament} from "../models/Tournament";
+import {ITournament, ITournamentPlayer} from "../models/Tournament";
 import {IGame} from "../models/Game";
+import {IUser} from "../models/User";
 
 export const startTournamentFinishedCleanerCron = async (fastify: FastifyInstance) => {
   /**
@@ -10,13 +11,20 @@ export const startTournamentFinishedCleanerCron = async (fastify: FastifyInstanc
    */
   fastify.cron.schedule('0 3 * * MON', async () => {
     try {
-      const lastTournament = await fastify.models.Tournament.findOne({ finished: true }).sort({ date: -1 }).populate('game') as ITournament & { game: IGame };
+      const lastTournament = await fastify.models.Tournament.findOne({ finished: true }).sort({ date: -1 }).populate('game').populate('players.user') as ITournament & { game: IGame, players: (ITournamentPlayer & { user: IUser; })[] };
       if (!lastTournament) {
         log(fastify, 'Aucun tournoi fini trouvé pour le nettoyage.', 'info');
         return;
       }
 
-      if (lastTournament.mvpVoteOpen) await fastify.discordService.closeTournament(lastTournament);
+      if (lastTournament.mvpVoteOpen) {
+        const mvp = lastTournament.players.reduce((prev, current) => ((prev?.mvpVotes.length ?? 0) > current.mvpVotes.length) ? prev : current, null as ITournamentPlayer | null);
+        if (mvp) await fastify.discordService.announceMvpWinner(lastTournament, mvp.id);
+        lastTournament.mvpVoteOpen = false;
+        await lastTournament.save();
+        log(fastify, `Fermeture automatique du vote mvp du tournoi : ${lastTournament.name}.`, 'info');
+      }
+      await fastify.discordService.closeTournament(lastTournament);
 
       log(fastify, `Nettoyage du tournoi : ${lastTournament.name}.`, 'info');
     } catch (error) {
